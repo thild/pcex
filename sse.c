b4bc6ac8090551773d65f79f77af7bede11a944b
rodrigont <rodrigo-n-t@hotmail.com>
2015-04-13 22:26:49 -0300

    Atividade2.c
    Enviou da atividade 2: Arquiteturas SIMD - Implementação.

--- sse.c
+++ sse.c
@@ -1,22 +1,3 @@
-/*
-Exemplo de código SSE utilizando Intel instrinsics e gcc built-in functions
-
-Compile usando:
-gcc sse.c -o sse -msse -msse4.2 -O3
-
-
-http://www.songho.ca/misc/sse/sse.html
-
-https://gcc.gnu.org/onlinedocs/gcc-4.9.2/gcc/X86-Built-in-Functions.html#X86-Built-in-Functions
-https://gcc.gnu.org/onlinedocs/gcc-4.9.2/gcc/Vector-Extensions.html#Vector-Extensions
-https://software.intel.com/sites/landingpage/IntrinsicsGuide/
-https://msdn.microsoft.com/en-us/library/26td21ds.aspx
-
-AVX
-https://software.intel.com/en-us/articles/introduction-to-intel-advanced-vector-extensions/
-
-*/
-
 #include <stdio.h>
 
 #include <xmmintrin.h> // SSE (Required to use the __m128, and __m128d type)
@@ -24,88 +5,40 @@ https://software.intel.com/en-us/articles/introduction-to-intel-advanced-vector-
 #include <pmmintrin.h> // SSE3
 #include <smmintrin.h> // SSE4.1
 
-
-#define VECTOR_SIZE         4
-typedef float v4sf __attribute__ ((vector_size(sizeof(float)*VECTOR_SIZE))); 
-
-typedef union f4vector
+int main ()
 {
-    v4sf    v;
-    float   f[VECTOR_SIZE];
-} f4vector;
+    int i, y, x;
+    int w = 8, h = 5;
+    __m128 c = {10, 10, 10, 10};
+    float *a __attribute__ ((aligned(16))) = (float*) malloc (sizeof(float) * (h * w));
+    float *b;
 
+    for(y = 0; y < h; ++y){
+		for(x = 0; x < w; ++x){
+            a[y*w+x] = x + y + 200;
+			printf("%.2f\t", a[y*w+x]);
+		}
+		printf("\n");
+	}
 
-void add_intel_intrinsics(float *a, float *b, float *c)
-{
-  __m128 va = _mm_load_ps (a);
-  __m128 vb = _mm_load_ps (b);
-  __m128 vc = _mm_add_ps (va, vb);
-  _mm_store_ps(c, vc);
+    b = a;
+    for(y = 0; y < h; ++y){
+		for(x = 0; x < w; ++x){
+			__m128 img = _mm_load_ps(b);
+			__m128 r = _mm_add_ps(img, c);
+			_mm_store_ps(b, r);
 
-  /* Equivalente Assembly 
-  ** mov eax, a
-  ** mov edx, b 
-  ** mov ecx, c
-  ** movaps xmm0, XMMWORD PTR [eax]
-  ** addps xmm0, XMMWORD PTR [edx]
-  ** movaps XMMWORD PTR [ecx], xmm0
-  */
-}
+			b += 4;
+		}
+    }
 
+    printf("\ndepois da execucaoo\n");
+    for(y = 0; y < h; ++y){
+		for(x = 0; x < w; ++x){
+			printf("%.2f ",a[y*w+x]);
+		}
+		printf("\n");
+	}
 
-
-v4sf add_gcc_builtin(v4sf a, v4sf b)
-{
-  return __builtin_ia32_addps (a, b);
-}
-
-
-
-int main (int argc, char *argv[])
-{
-  float *a __attribute__ ((aligned(16))) = (float*)malloc (sizeof(float) * 4); //aloca um vetor de 16bytes (128bits) alinhado em endereços múltiplos de 16bytes.
-  float *b __attribute__ ((aligned(16))) = (float*)malloc (sizeof(float) * 4);
-  float *c __attribute__ ((aligned(16))) = (float*)malloc (sizeof(float) * 4);
-  
-  int i = 0;
-  
-  for (i = 0; i < 4; ++i) {
-    a[i] = i;
-    b[i] = i;
-  }
-  
-  printf("Intel SSE\n");
-  
-  add_intel_intrinsics(a, b, c);
-
-  for (i = 0; i < 4; ++i) {
-    printf("%f\n", c[i]);
-  }
-  
-  free(a);
-  free(b);
-  free(c);
-  
-  printf("\nGCC Built-in Functions\n");
-  
-  v4sf d, e, f;
-
-  d = (v4sf){0, 1, 2, 3};
-  e = (v4sf){0, 1, 2, 3};
-  f = add_gcc_builtin(d, e);  
-
-  for (i = 0; i < 4; ++i) {
-    printf("%f\n", f[i]);
-  }
-  
-  
-  printf("\nGCC implicity vectorization\n");
-  
-  f = d + e;  
-
-  for (i = 0; i < 4; ++i) {
-    printf("%f\n", f[i]);
-  }
-
-  return 0;
+    return 0;
 }
